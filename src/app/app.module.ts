4c232f5d56f11aca29301d137feb8c93438b6f03
Gaurav J. <gaurav.joshi@email.com>
2018-02-21 02:22:55 +0530

    layout and ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
+import { FlexLayoutModule } from '@angular/flex-layout;
+
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, BsDropdownModule.forRoot()
+    BrowserModule, MatButtonModule, MatCheckboxModule, FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
